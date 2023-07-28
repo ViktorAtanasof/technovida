c7d5a873047d12edd8acbd6a024479dbedd8c4f9
Plamen Atanasov <atanasowviktor@gmail.com>
2023-07-28 17:27:20 +0300

    Create Sidenav component

---
+++ app/src/components/SideNav/SideNav.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import './SideNav.css';
+import { Link } from 'react-router-dom';
+import { FetchCategories } from '../../services/FetchCategories';
+
+function SideNav({
+    isOpen,
+    onCloseSidenav
+}) {
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const categories = FetchCategories();
+
+    const handleCategorySelection = (category) => {
+        setSelectedCategory(category);
+    };
+
+    const handleSubCategoryClick = () => {
+        setSelectedCategory(null);
+        onCloseSidenav(); 
+    }
+
+    return (
+        <div className={`sidenav ${isOpen ? 'open' : ''}`}>
+            <div className="sidenavHeader">
+                <h2>Choose a category</h2>
+            </div>
+            {isOpen && (
+                <div className='category-menu'>
+                    <ul>
+                        {categories.map((category) => (
+                            <li
+                                key={category.id}
+                                onClick={() => handleCategorySelection(category)}
+                                className={selectedCategory === category ? 'active' : ''}
+                            >
+                                {category.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+            {selectedCategory && (
+                <div className='subcategory-menu'>
+                    <h3>Subcategories for <span>{selectedCategory.name}</span>:</h3>
+                    <ul>
+                        {selectedCategory.subcategories?.map((subcategory, index) => (
+                            <li key={index} onClick={handleSubCategoryClick} >
+                                <Link to={`/${selectedCategory.name.toLowerCase()}/${subcategory.toLowerCase()}`}>
+                                    {subcategory}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default SideNav;
