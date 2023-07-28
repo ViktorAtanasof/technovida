966b74ed346e565a004e4879687f2261cb02b125
Plamen Atanasov <atanasowviktor@gmail.com>
2023-07-28 17:25:59 +0300

    Create logic that handles routing to sub-categories and logic that loads default category upon initial load

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import SubCategory from './components/SubCategory/SubCategory';
+import { useEffect } from 'react';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Default page is phones so redirect to /phones/phones if the current path is '/'
+    if (window.location.pathname === '/') {
+      navigate('/phones/phones');
+    }
+  }, [navigate]);
+
   return (
-    <Header />
+    <>
+      <Header />
+      <Routes>
+        <Route path='/:category/:subCategory' element={<SubCategory />} />
+      </Routes>
+    </>
   );
 }
 
