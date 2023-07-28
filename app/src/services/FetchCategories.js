ae13037dbefc1932b2d382fe59889f48b10ff0c8
Plamen Atanasov <atanasowviktor@gmail.com>
2023-07-28 17:26:56 +0300

    Create service that fetches all categories

---
+++ app/src/services/FetchCategories.js
@@ -0,0 +1,23 @@
+import { db } from '../firebase';
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from 'react';
+
+export const FetchCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'categories'));
+        const categoriesData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setCategories(categoriesData);
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  return categories;
+};
