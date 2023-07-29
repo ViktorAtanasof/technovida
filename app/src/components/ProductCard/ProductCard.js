db479479d1e505065fc70102e1539540f441a2a2
Plamen Atanasov <atanasowviktor@gmail.com>
2023-07-29 17:24:08 +0300

    Create ProductCard component and dynamically visualise data

---
+++ app/src/components/ProductCard/ProductCard.js
@@ -0,0 +1,30 @@
+import '../ProductCard/ProductCard.css';
+import StarRating from '../StarRating/StarRating';
+
+function ProductCard({
+    id,
+    product
+}) {
+    const shortenDesc = product.description.slice(0, 80) + '...';
+
+    return (
+        <li className="product__card">
+            <img className="product__image" src={product.image} alt="product" />
+            <div className="product__details">
+                <p className="product__name">{product.name}</p>
+                <StarRating averageRating={product.ratings} />
+                <p className="product__desc">{shortenDesc}</p>
+                <div className="prices">
+                    {product.oldprice && (
+                        <p className="product__old-price">{product.oldprice}<i className="fa-solid fa-dollar-sign"></i></p>
+                    )}
+                    <p className="product__price">{product.price}<i className="fa-solid fa-dollar-sign"></i></p>
+                </div>
+            </div>
+            <button className='cart-btn' onClick={() => alert('Product added to cart')}>Add to cart</button>
+        </li>
+
+    );
+}
+
+export default ProductCard;
