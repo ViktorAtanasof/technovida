1043b8e5466705a37013cf78b4ef56fd00901eef
ViktorAtanasof <atanasowviktor@gmail.com>
2023-07-31 15:05:39 +0300

    Refactor code: Optimizations

--- app/src/components/HamburgerButton/HamburgerButton.js
+++ app/src/components/HamburgerButton/HamburgerButton.js
@@ -2,27 +2,26 @@ import { useState } from 'react';
 import '../HamburgerButton/HamburgerButton.css';
 import SideNav from '../SideNav/SideNav';
 
+const Overlay = ({ onClick }) => <div className='overlay' onClick={onClick}></div>;
+const CloseButton = ({ onClick }) => <div className='closeBtn' onClick={onClick}>&times;</div>;
+
 function HamburgerButton() {
     const [navOpen, setNavOpen] = useState(false);
 
-    const openNav = () => {
-        setNavOpen(true);
-    }
-
-    const closeNav = () => {
-        setNavOpen(false);
-    }
+    const toggleNav = () => {
+        setNavOpen((prevState) => !prevState);
+    };
 
     return (
         <>
-            <div className="hamburger-btn" onClick={openNav}>
+            <div className="hamburger-btn" onClick={toggleNav}>
                 &#9776;
             </div>
             {navOpen && (
                 <>
-                    <SideNav isOpen={navOpen} onCloseSidenav={closeNav} />
-                    <div className='overlay' onClick={closeNav}></div>
-                    <div className='closeBtn' onClick={closeNav}>&times;</div>
+                    <SideNav isOpen={navOpen} onCloseSidenav={toggleNav} />
+                    <Overlay onClick={toggleNav} />
+                    <CloseButton onClick={toggleNav} />
                 </>
             )}
         </>
