1405ca693d6d520ea872034fc1128c28d469d5aa
ViktorAtanasof <atanasowviktor@gmail.com>
2023-08-15 13:55:17 +0300

    Create Spinner component for visualizing loading animations

---
+++ app/src/components/Spinner/Spinner.js
@@ -0,0 +1,13 @@
+import '../Spinner/Spinner.css';
+
+function Spinner() {
+    return (
+        <div className="container">
+            <div className="loader"></div>
+            <div className="loader"></div>
+            <div className="loader"></div>
+        </div>
+    );
+};
+
+export default Spinner;
